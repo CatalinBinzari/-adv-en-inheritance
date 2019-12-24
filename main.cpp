46df5f961e827951b378d0c3f7a1bb8a99cc5bb5
CatalinBinzari <48354874+CatalinBinzari@users.noreply.github.com>
2019-12-24 15:02:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "Player.h"
 int main(){
  	Monster lena{Monster::DRAGON,"dragon",'D',20,4,100};
- 	Monster olga{Monster::ORC,"orc",'o',4,2,25};
+ 	Monster sergei{Monster::ORC,"orc",'o',4,2,25};
  	Monster sli{Monster::SLIME,"slime",'s',1,1,10};
  	cout<<lena.getSymbol()<<endl;
 
